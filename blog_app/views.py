33600b91b8bb116e909afd0ce9f6ac8ce0a62534
Shuvo dev nath <shuvodevnath0188@gmail.com>
2026-05-22 20:28:21 +0600

    Post list

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,19 @@
+from typing import Any
+
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView
+from .models import Author, Post, Comment
 
 # Create your views here.
+
+class PostList(ListView):
+    template_name = 'post_list.html'
+    model = Post
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context =  super().get_context_data(**kwargs)
+        context['post_list'] = Post.objects.all()
+        return context
+    
+    
